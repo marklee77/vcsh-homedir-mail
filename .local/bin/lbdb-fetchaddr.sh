b500f01f5636c0301e24338163dd6edd3bf03edd
Mark Stillwell <mark@stillwell.me>
2015-04-28 10:52:17 +0100

    autocommit

--- .local/bin/lbdb-fetchaddr.sh
+++ .local/bin/lbdb-fetchaddr.sh
@@ -5,19 +5,19 @@ emailfile="${HOME}/.lbdb/m_inmail.list"
 mkdir -p $(dirname $emailfile)
 touch $emailfile
 
-$fetchaddr | while IFS=$'\t' read addr name date; do
+$fetchaddr | perl -pe 's| while IFS=$'\t' read addr name date; do
     # lowercase addr
     addr=${addr,,}
 
     # quote name if it contains a ",", but don't double quote
     [[ "$name" =~ ^[^\"].*,.*[^\"]$ ]] && name="\"$name\""
 
-    datestamp=$(date -d "$date" +%s)
+    datestamp=$(date -d "$date" +%s) || echo -e "DATESTAMP: $addr\t$name\t$date"
 
     prevdate=$(perl -lane "print \"\$F[-2] \$F[-1]\" if \$F[0] eq '$addr'" \
                $emailfile | tail -1)
     prevstamp=0
-    [ -n "$prevdate" ] && prevstamp=$(date -d "$prevdate" +%s)
+    [ -n "$prevdate" ] && (prevstamp=$(date -d "$prevdate" +%s) || echo -e "PREVDATESTAMP: $addr\t$name\t$date")
     
     # if current is newer then delete old from file and replace
     if [ $datestamp -gt $prevstamp ]; then
