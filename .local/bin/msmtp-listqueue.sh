9a7094f99deae1e16662a8dde586034aa59a66d3
Mark Stillwell <mark@stillwell.me>
2015-10-27 01:03:43 +0000

    autocommit

--- .local/bin/msmtp-listqueue.sh
+++ .local/bin/msmtp-listqueue.sh
@@ -8,7 +8,8 @@ if ! ls ${queuedir}/*.msmtp >/dev/null 2>/dev/null; then
 fi
 
 for file in ${queuedir}/*.msmtp; do
-    egrep -s --colour -h '(^From:|^To:|^Subject:)' "$file"
+    #egrep -s --colour -h '(^From:|^To:|^Subject:)' "$file"
+    perl -ne 'if (/^\s*$/) { exit; } elsif (/^From:|^To:|^Subject:/) { print; }' < "$file"
     echo " "
 done
 
