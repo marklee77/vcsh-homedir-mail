945e68037541bb416ccbe92d373bcd3646eccf31
Mark Stillwell <mark@stillwell.me>
2016-09-16 13:37:58 +0100

    autocommit

--- .local/bin/msmtp-runqueue.sh
+++ .local/bin/msmtp-runqueue.sh
@@ -42,9 +42,6 @@ while getopts m: opt; do
 done
 shift $(($OPTIND-1))
 
-# enable socks proxy forwarding
-#. socksify
-
 # process all mails
 failcount=0
 find $queuedir -type f -name "*.msmtp" $msmtpargs | while read file; do
