09f5a254c55e10c479824fae4a6bcb64d7dda218
Mark Stillwell <mark@stillwell.me>
2017-07-17 20:42:07 -0700

    autocommit

--- .local/bin/msmtp-runqueue.sh
+++ .local/bin/msmtp-runqueue.sh
@@ -1,60 +1,53 @@
 #!/bin/bash
-scriptname="$(basename $0)"
-
-lockdir="${XDG_RUNTIME_DIR:-${HOME}/.local/var/lock}/${scriptname}.lock"
-pidfile="${lockdir}/PID"
-if mkdir ${lockdir} &> /dev/null; then
-    trap "rm -rf ${lockdir}" EXIT SIGHUP SIGINT SIGQUIT SIGTERM
-    echo "$$" > ${pidfile}
-else
-    otherpid=$(cat ${pidfile} 2>/dev/null)
-    othercmd=$(ps --no-headers --format command --pid ${otherpid} 2>/dev/null)
-    if [[ "${othercmd}" =~ .*${scriptname}.* ]]; then
-        if [ $(ps --no-headers -C ${scriptname} | wc -l) -ge 10 ]; then
-            echo "too many ${scriptname} processes waiting for lock"
-            exit 1
-        fi
-        sleep 1
-    else
-        rm -rf ${lockdir}
-    fi
-    exec "$0" "$@"
+scriptname="$(basename "${BASH_SOURCE[0]}")"
+rundir="${XDG_RUNTIME_DIR:-${HOME}/.local/var/run}/${scriptname}"
+pidfile="${rundir}/PID"
+cleanup () {
+    rm -f "${pidfile}"
+    flock -u 200
+}
+mkdir -p "${rundir}"
+exec 200>"${pidfile}"
+if ! flock -n 200 || ! echo "$$" > "${pidfile}"; then
+    echo "another instance of the ${scriptname} is currently running..." >&2
+    exit 1
 fi
+trap cleanup EXIT HUP INT QUIT TERM
 
 # Set secure permissions on created directories and files
 umask 077
 
-queuedir=${HOME}/.local/var/spool/msmtpqueue
-mkdir -p $queuedir || exit 1
-cd $queuedir || exit 1
+queuedir="${HOME}/.local/var/spool/msmtpqueue"
+mkdir -p "${queuedir}"
+cd "${queuedir}" || exit 1
 
-if ! ls *.msmtp >/dev/null 2>/dev/null; then
+if ! find "${queuedir}" -name '*.msmtp' | read -r; then
     echo "No mails in $queuedir"
     exit 0
 fi
 
 msmtpargs=""
 while getopts m: opt; do
-    case $opt in
+    case "${opt}" in
         m)
-        msmtpargs="$msmtpargs -mmin +$OPTARG"
+        msmtpargs="${msmtpargs} -mmin +$OPTARG"
     esac
 done
-shift $(($OPTIND-1))
+shift "$((OPTIND-1))"
 
 # process all mails
 failcount=0
-find $queuedir -type f -name "*.msmtp" $msmtpargs | while read file; do
-    echo "*** Sending $file to $(perl -lne 'print $1 if $. == 1 and /-- (.*)$/' $file) ..."
-    if perl -ne 'print if $. > 1' $file | msmtp $(head -1 $file) $@; then
-        rm -f $file
+while read -r file; do
+    echo "*** Sending $file to $(perl -lne 'print $1 if $. == 1 and /-- (.*)$/' "${file}") ..."
+    if perl -ne 'print if $. > 1' "${file}" | msmtp $(head -1 "${file}") "$@"; then
+        rm -f "${file}"
         echo "$file sent successfully"
     else
         echo "FAILURE"
-        failcount=$(($failcount+1))
+        failcount="$((failcount+1))"
     fi
-done
+done < <(find "${queuedir}" -name '*.msmtp' ${msmtpargs})
 
 pkill -RTMIN+4 i3blocks
 
-exit $failcount
+exit "${failcount}"
