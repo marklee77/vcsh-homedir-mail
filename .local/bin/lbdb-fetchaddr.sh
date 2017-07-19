c67e0315920d655d539b42a76c86f768f1c5dbfa
Mark Stillwell <mark@stillwell.me>
2017-07-18 20:46:22 -0700

    autocommit

--- .local/bin/lbdb-fetchaddr.sh
+++ .local/bin/lbdb-fetchaddr.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
+scriptname="$(basename "${BASH_SOURCE[0]}")"
+rundir="${XDG_RUNTIME_DIR:-${HOME}/.local/var/run}/${scriptname}"
+pidfile="${rundir}/PID"
+cleanup () {
+    rm -f "${pidfile}"
+    flock -u 200
+}
+mkdir -p "${rundir}"
+exec 200>"${pidfile}"
+if ! flock -w 5 200 || ! echo "$$" > "${pidfile}"; then
+    echo "another instance of the ${scriptname} is holding the lock..." >&2
+    exit 1
+fi
+trap cleanup EXIT HUP INT QUIT TERM
+
 fetchaddr=/usr/lib/lbdb/fetchaddr
 emailfile="${HOME}/.lbdb/m_inmail.list"
 
-if [ -f "$1" ]; then
-    if ! [[ "$(file -bi "$1")" =~ ^message/rfc822\; ]]; then
-        exit 0
-    fi
-    exec < "$1"
-fi
+[ -f "$1" ] && exec <"$1"
 
 mkdir -p "$(dirname "${emailfile}")"
 touch "${emailfile}"
 
 headers="$(reformail -X from: -X to: -X cc: -X bcc: -X date:)"
 
+encoding="$(file -bi - <<< "${headers}" | sed -nr 's/.*\scharset=(.*)/\1/p')"
+headers="$(iconv -f "${encoding}" -t utf-8 <<< "${headers}" 2>/dev/null)"
 msgdate="$(date -d "$(reformail -x date: <<< "$headers")" +"%Y-%m-%d %H:%M" 2>/dev/null)"
-encoding="$(file -bi - <<< "${headers}" | perl -ne 'print $1 if /charset=(.*)/')"
-reencoded_headers="$(iconv -f "${encoding}" -t utf-8 <<< "${headers}" 2>/dev/null)"
-headers="${reencoded_headers}"
 
-"${fetchaddr}" -c utf-8 <<< "${headers}" | while IFS=$'\t' read -r -a fields; do
+while IFS=$'\t' read -r -a fields; do
 
     addr="${fields[0]//\'}"
-    name="${fields[*]:1:$((${#fields[@]}-2))}"
+    name="${fields[*]:1:$((${#fields[*]}-2))}"
     date="${fields[-1]}"
 
     # lowercase addr
@@ -32,15 +41,14 @@ headers="${reencoded_headers}"
 
     datestamp="$(date -d "${date}" +%s)"
 
-    prevdate=$(perl -lane "print \"\$F[-2] \$F[-1]\" if \$F[0] eq '$addr'" \
-               $emailfile | tail -1)
+    prevdate="$(sed -nr "s/^${addr}\s.*\s(\S+\s+\S+)\$/\1/p" "${emailfile}" | tail -1 )"
     prevstamp=0
     [ -n "$prevdate" ] && prevstamp=$(date -d "$prevdate" +%s)
 
     # if current is newer then delete old from file and replace
-    if [ $datestamp -gt $prevstamp ]; then
-        perl -i -ane "print unless \$F[0] eq '$addr'" $emailfile
-        echo -e "$addr\t$name\t$date" >> $emailfile
+    if [ "${datestamp}" -gt "${prevstamp}" ]; then
+        sed -i "/^${addr}\s/d" "${emailfile}"
+        echo -e "${addr}\t${name}\t${date}" >> "${emailfile}"
     fi
 
-done
+done < <("${fetchaddr}" -c utf-8 <<< "${headers}")
